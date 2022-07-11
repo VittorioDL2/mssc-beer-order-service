932816eb6c8fffd0c5ae401c4dad70ddb075e9f9
REPLYNET\v.dileo <v.dileo@reply.it>
2022-07-11 18:31:53 +0200

    wip

---
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -0,0 +1,20 @@
+package guru.sfg.beer.order.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter mj2mc = new MappingJackson2MessageConverter();
+		mj2mc.setTargetType(MessageType.TEXT);
+		mj2mc.setTypeIdPropertyName("_type");
+		return mj2mc;
+	}
+
+}
